5d645c205ad9d57aefcc55536e402aa8fedf9f25
robertjchandler <robertjchandler4@gmail.com>
2026-07-12 16:53:14 -0500

    update server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -17,16 +18,36 @@ func main() {
 	conn, _ := amqp.Dial(connectionString)
 
 	ch, _ := conn.Channel()
-	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
-
 	defer conn.Close()
 
 	fmt.Println("Connection to Peril server successful.")
 
-	// wait for ctrl+c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+	gamelogic.PrintServerHelp()
+
+outerLoop:
+	for {
+		userinput := gamelogic.GetInput()
+		if len(userinput) == 0 {
+			continue
+		}
+		switch userinput[0] {
+		case "pause":
+			fmt.Println("Sending pause message.")
+			pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+		case "resume":
+			fmt.Println("Sending resume message.")
+			pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+		case "quit":
+			fmt.Println("Exiting game.")
+			break outerLoop
+		default:
+			fmt.Println("Command not understood.")
+		}
+		// wait for ctrl+c
+		signalChan := make(chan os.Signal, 1)
+		signal.Notify(signalChan, os.Interrupt)
+		<-signalChan
+	}
 
 	fmt.Println("Peril shutting down.")
 	conn.Close()
